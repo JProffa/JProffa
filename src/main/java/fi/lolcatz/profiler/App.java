cdbcebd6992820d33151be9c5d36a34fec9e933d
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2012-09-09 17:18:24 +0300

    Style fix and Jenkins hook test
    Fixed style of code. This commit also tests if Jenkins notices the push
    and builds the project.

--- src/main/java/fi/lolcatz/profiler/App.java
+++ src/main/java/fi/lolcatz/profiler/App.java
@@ -4,10 +4,9 @@ package fi.lolcatz.profiler;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
