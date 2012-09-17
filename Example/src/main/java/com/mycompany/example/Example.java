e378365d4987d603bc3a18167d608e6d4ac514e1
Hallo <ville.knuuttila@gmail.com>
2012-09-17 11:06:20 +0300

    added psvm to example.java

--- Example/src/main/java/com/mycompany/example/Example.java
+++ Example/src/main/java/com/mycompany/example/Example.java
@@ -2,6 +2,11 @@ package com.mycompany.example;
 
 public class Example {
 
+    public static void main(String[] args) {
+        sum(1, 2);
+    }
+    
+    
     public static int sum(int a, int b) {
         return a + b;
     }
