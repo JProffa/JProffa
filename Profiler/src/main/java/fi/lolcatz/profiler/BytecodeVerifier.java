9bec35c3799abd9256451afa9f3aae7055c2ac42
Logima <logimaru@gmail.com>
2012-10-01 18:15:19 +0300

    Modified BytecodeVerifier.java to work without modified ASM, reverted pom.xml to use original asm-all-4.0, deleted modified asm-4.0-profiler

--- Profiler/src/main/java/fi/lolcatz/profiler/BytecodeVerifier.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/BytecodeVerifier.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 import org.objectweb.asm.MethodVisitor;
 import static org.objectweb.asm.Opcodes.*;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
@@ -55,12 +56,17 @@ public class BytecodeVerifier extends MethodVisitor {
         mn.accept(next);
     }
 
-    /*
+    /**
      * A custom made class to represent Frame objects.
      */
     public class Node extends Frame {
 
         Set<Node> successors = new HashSet<Node>();
+        
+        /**
+        * The index of the instruction node.
+        */
+        int insnIndex;
 
         public Node(int nLocals, int nStack) {
             super(nLocals, nStack);
@@ -71,38 +77,42 @@ public class BytecodeVerifier extends MethodVisitor {
         }
     }
 
-    /*
+    /**
      * A class for counting the cyclomatic complexity of a method. ASM pdf 8.2.5.
      */
     public class CyclomaticComplexity {
 
         public int getCyclomaticComplexity(String owner, MethodNode mn)
                 throws AnalyzerException {
-            Analyzer a =
-                    new Analyzer(new BasicInterpreter()) {
-                        /*
-                         * Constructs the control flow graph by representing Frames as Nodes.
-                         */
-                        protected Frame newFrame(int nLocals, int nStack) {
-                            return new Node(nLocals, nStack);
-                        }
+            Analyzer a = new Analyzer(new BasicInterpreter()) {
+                /**
+                 * Constructs the control flow graph by representing Frames as
+                 * Nodes.
+                 */
+                @Override
+                protected Frame newFrame(int nLocals, int nStack) {
+                    return new Node(nLocals, nStack);
+                }
 
-                        protected Frame newFrame(
-                                Frame src) {
-                            return new Node(src);
-                        }
-                        /*
-                         * Overrides the method to display edges as successors of a node.
-                         * @Param src source node
-                         * @Param dst destination node
-                         */
+                @Override
+                protected Frame newFrame(Frame src) {
+                    return new Node(src);
+                }
+                
+                /*
+                 * Overrides the method to display edges as successors of a node and store instruction index.
+                 * @Param src source node
+                 * @Param dst destination node
+                 */
+                @Override
+                protected void newControlFlowEdge(int src, int dst) {
+                    Node s = (Node) getFrames()[src];
+                    Node successor = (Node) getFrames()[dst];
+                    successor.insnIndex = src;
+                    s.successors.add(successor);
 
-                        protected void newControlFlowEdge(int src, int dst) {
-                            Node s = (Node) getFrames()[src];
-                            s.successors.add((Node) getFrames()[dst]);
-
-                        }
-                    };
+                }
+            };
             //Analyze the method, initializing the frame array
             a.analyze(owner, mn);
             mn.maxStack += 4;
@@ -114,6 +124,7 @@ public class BytecodeVerifier extends MethodVisitor {
             counterIncreaseInsn.add(new InsnNode(LADD));
             counterIncreaseInsn.add(new FieldInsnNode(PUTSTATIC, "com/mycompany/example/Example", "counter", "J"));
             mn.instructions.insert(counterIncreaseInsn);
+            int numOfInsertedIncrementors = 1;
             
             Frame[] frames = a.getFrames();
             
@@ -133,7 +144,8 @@ public class BytecodeVerifier extends MethodVisitor {
                             counterIncreaseInsn.add(new InsnNode(LCONST_1));
                             counterIncreaseInsn.add(new InsnNode(LADD));
                             counterIncreaseInsn.add(new FieldInsnNode(PUTSTATIC, "com/mycompany/example/Example", "counter", "J"));
-                            mn.instructions.insertBefore(successor.getIns(), counterIncreaseInsn);
+                            mn.instructions.insertBefore(mn.instructions.get(successor.insnIndex + numOfInsertedIncrementors * 8), counterIncreaseInsn);
+                            numOfInsertedIncrementors++;
                         }
                     }
                     nodes += 1;
