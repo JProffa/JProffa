4c65dd82abf2cebcca678e1b674fed441d134bff
Logima <logimaru@gmail.com>
2012-10-01 17:06:11 +0300

    Added counter increments before every basic block, changed pom.xml to use our modified asm-4.0-profiler in a really ugly way (requires symlink)

--- Example/src/main/java/com/mycompany/example/Example.java
+++ Example/src/main/java/com/mycompany/example/Example.java
@@ -1,13 +1,20 @@
 package com.mycompany.example;
 
 public class Example {
+    
+    public static long counter = 0;
 
     public static void main(String[] args) {
         sum(1, 2);
+        System.out.println("count: " + counter);
     }
     
     
     public static int sum(int a, int b) {
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+            x++;
+        }
         return a + b;
     }
 }
