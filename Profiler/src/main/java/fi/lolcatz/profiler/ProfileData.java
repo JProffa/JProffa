d4a99472704ec4a7cef40fbc309d68c6154789e3
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2012-10-08 07:49:17 +0300

    Alternative basic block counting implementation

--- Profiler/src/main/java/fi/lolcatz/profiler/ProfileData.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/ProfileData.java
@@ -95,7 +95,6 @@ public class ProfileData {
         for (int i = 0; i < callsToBasicBlock.length; i++) {
             System.out.println(i + ": Calls: " + callsToBasicBlock[i] + " Cost: " + basicBlockCost[i] + " Total: " +
                     callsToBasicBlock[i] * basicBlockCost[i]);
-            System.out.printf("%d: %d %d", args)
         }
     }
 }
