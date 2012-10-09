63bb1887d719457d9b9c2e0c1e15ab96f33c844d
Hallo <ville.knuuttila@gmail.com>
2012-10-09 15:49:42 +0300

    Added tests to test our Example methods efficiency

--- Example/src/test/java/com/mycompany/example/AppTest.java
+++ Example/src/test/java/com/mycompany/example/AppTest.java
@@ -30,13 +30,34 @@ public class AppTest {
     public void testRecursionCostSix() {
         FunctionExample.recursiveFunction(6);
         long totalCost = ProfileData.getTotalCost();
-        assertTrue(200 > totalCost && totalCost > 100 );
+        assertTrue("The total cost was: " + totalCost, 200 > totalCost && totalCost > 100 );
     }
 
     @Test
     public void testRecursionCostThree() {
         FunctionExample.recursiveFunction(3);
         long totalCost = ProfileData.getTotalCost();
-        assertTrue(170 > totalCost && totalCost > 85);
+        assertTrue("The total cost was: " + totalCost, 170 > totalCost && totalCost > 85);
+    }
+    
+    @Test
+    public void testIterativeCostFive(){
+        FunctionExample.iterativeFunction(5);
+        long totalCost = ProfileData.getTotalCost();
+        assertTrue("The total cost was: " + totalCost, 150 > totalCost && totalCost > 50);
+    }
+    
+    @Test
+    public void testIterativeHundred(){
+        FunctionExample.iterativeFunction(100);
+        long totalCost = ProfileData.getTotalCost();
+        assertTrue("The total cost was: " + totalCost, 400 > totalCost && totalCost > 300);
+    }
+    
+    @Test
+    public void testSumOnePlusTwo() {
+        Example.sum(1, 2);
+        long totalCost = ProfileData.getTotalCost();
+        assertTrue("The total cost was: " + totalCost, 70 > totalCost && totalCost > 60);
     }
 }
