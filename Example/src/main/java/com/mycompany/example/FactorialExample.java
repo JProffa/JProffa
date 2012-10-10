0f492233ab58abd9d4889e4339067cb649944b80
Hallo <ville.knuuttila@gmail.com>
2012-10-10 10:28:38 +0300

    Added more test to Example functions

--- Example/src/main/java/com/mycompany/example/FactorialExample.java
+++ Example/src/main/java/com/mycompany/example/FactorialExample.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.example;
 
-/**
- *
- * @author oorissan
- */
+
 public class FactorialExample {
     
     public static int factorialInt(int number){
