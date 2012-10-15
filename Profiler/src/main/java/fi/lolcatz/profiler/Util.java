68b10b47ce0cb045e172020bc1e29e908d437c61
Logima <logimaru@gmail.com>
2012-10-15 16:39:27 +0300

    Added all available bytecodes to Util.getOpcodeString()

--- Profiler/src/main/java/fi/lolcatz/profiler/Util.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/Util.java
@@ -127,20 +127,11 @@ public class Util implements Opcodes {
     public static String getOpcodeString(int opcode) {
         String s;
         switch (opcode) {
-            case ALOAD:
-                s = "ALOAD";
+            case NOP:
+                s = "NOP";
                 break;
-            case INVOKESPECIAL:
-                s = "INVOKESPECIAL";
-                break;
-            case INVOKESTATIC:
-                s = "INVOKESTATIC";
-                break;
-            case RETURN:
-                s = "RETURN";
-                break;
-            case IRETURN:
-                s = "IRETURN";
+            case ACONST_NULL:
+                s = "ACONST_NULL";
                 break;
             case ICONST_M1:
                 s = "ICONST_M1";
@@ -154,30 +145,460 @@ public class Util implements Opcodes {
             case ICONST_2:
                 s = "ICONST_2";
                 break;
-            case ISTORE:
-                s = "ISTORE";
+            case ICONST_3:
+                s = "ICONST_3";
                 break;
-            case ILOAD:
-                s = "ILOAD";
+            case ICONST_4:
+                s = "ICONST_4";
                 break;
-            case IINC:
-                s = "IINC";
+            case ICONST_5:
+                s = "ICONST_5";
                 break;
-            case IADD:
-                s = "IADD";
+            case LCONST_0:
+                s = "LCONST_0";
+                break;
+            case LCONST_1:
+                s = "LCONST_1";
+                break;
+            case FCONST_0:
+                s = "FCONST_0";
+                break;
+            case FCONST_1:
+                s = "FCONST_1";
+                break;
+            case FCONST_2:
+                s = "FCONST_2";
+                break;
+            case DCONST_0:
+                s = "DCONST_0";
+                break;
+            case DCONST_1:
+                s = "DCONST_1";
                 break;
             case BIPUSH:
                 s = "BIPUSH";
                 break;
-            case IF_ICMPGE:
-                s = "IF_ICMPGE";
+            case SIPUSH:
+                s = "SIPUSH";
+                break;
+            case LDC:
+                s = "LDC";
+                break;
+            case ILOAD:
+                s = "ILOAD";
+                break;
+            case LLOAD:
+                s = "LLOAD";
+                break;
+            case FLOAD:
+                s = "FLOAD";
+                break;
+            case DLOAD:
+                s = "DLOAD";
+                break;
+            case ALOAD:
+                s = "ALOAD";
+                break;
+            case IALOAD:
+                s = "IALOAD";
+                break;
+            case LALOAD:
+                s = "LALOAD";
+                break;
+            case FALOAD:
+                s = "FALOAD";
+                break;
+            case DALOAD:
+                s = "DALOAD";
+                break;
+            case AALOAD:
+                s = "AALOAD";
+                break;
+            case BALOAD:
+                s = "BALOAD";
+                break;
+            case CALOAD:
+                s = "CALOAD";
+                break;
+            case SALOAD:
+                s = "SALOAD";
+                break;
+            case ISTORE:
+                s = "ISTORE";
+                break;
+            case LSTORE:
+                s = "LSTORE";
+                break;
+            case FSTORE:
+                s = "FSTORE";
+                break;
+            case DSTORE:
+                s = "DSTORE";
+                break;
+            case ASTORE:
+                s = "ASTORE";
+                break;
+            case IASTORE:
+                s = "IASTORE";
+                break;
+            case LASTORE:
+                s = "LASTORE";
+                break;
+            case FASTORE:
+                s = "FASTORE";
+                break;
+            case DASTORE:
+                s = "DASTORE";
+                break;
+            case AASTORE:
+                s = "AASTORE";
+                break;
+            case BASTORE:
+                s = "BASTORE";
+                break;
+            case CASTORE:
+                s = "CASTORE";
+                break;
+            case SASTORE:
+                s = "SASTORE";
                 break;
             case POP:
                 s = "POP";
                 break;
+            case POP2:
+                s = "POP2";
+                break;
+            case DUP:
+                s = "DUP";
+                break;
+            case DUP_X1:
+                s = "DUP_X1";
+                break;
+            case DUP_X2:
+                s = "DUP_X2";
+                break;
+            case DUP2:
+                s = "DUP2";
+                break;
+            case DUP2_X1:
+                s = "DUP2_X1";
+                break;
+            case DUP2_X2:
+                s = "DUP2_X2";
+                break;
+            case SWAP:
+                s = "SWAP";
+                break;
+            case IADD:
+                s = "IADD";
+                break;
+            case LADD:
+                s = "LADD";
+                break;
+            case FADD:
+                s = "FADD";
+                break;
+            case DADD:
+                s = "DADD";
+                break;
+            case ISUB:
+                s = "ISUB";
+                break;
+            case LSUB:
+                s = "LSUB";
+                break;
+            case FSUB:
+                s = "FSUB";
+                break;
+            case DSUB:
+                s = "DSUB";
+                break;
+            case IMUL:
+                s = "IMUL";
+                break;
+            case LMUL:
+                s = "LMUL";
+                break;
+            case FMUL:
+                s = "FMUL";
+                break;
+            case DMUL:
+                s = "DMUL";
+                break;
+            case IDIV:
+                s = "IDIV";
+                break;
+            case LDIV:
+                s = "LDIV";
+                break;
+            case FDIV:
+                s = "FDIV";
+                break;
+            case DDIV:
+                s = "DDIV";
+                break;
+            case IREM:
+                s = "IREM";
+                break;
+            case LREM:
+                s = "LREM";
+                break;
+            case FREM:
+                s = "FREM";
+                break;
+            case DREM:
+                s = "DREM";
+                break;
+            case INEG:
+                s = "INEG";
+                break;
+            case LNEG:
+                s = "LNEG";
+                break;
+            case FNEG:
+                s = "FNEG";
+                break;
+            case DNEG:
+                s = "DNEG";
+                break;
+            case ISHL:
+                s = "ISHL";
+                break;
+            case LSHL:
+                s = "LSHL";
+                break;
+            case ISHR:
+                s = "ISHR";
+                break;
+            case LSHR:
+                s = "LSHR";
+                break;
+            case IUSHR:
+                s = "IUSHR";
+                break;
+            case LUSHR:
+                s = "LUSHR";
+                break;
+            case IAND:
+                s = "IAND";
+                break;
+            case LAND:
+                s = "LAND";
+                break;
+            case IOR:
+                s = "IOR";
+                break;
+            case LOR:
+                s = "LOR";
+                break;
+            case IXOR:
+                s = "IXOR";
+                break;
+            case LXOR:
+                s = "LXOR";
+                break;
+            case IINC:
+                s = "IINC";
+                break;
+            case I2L:
+                s = "I2L";
+                break;
+            case I2F:
+                s = "I2F";
+                break;
+            case I2D:
+                s = "I2D";
+                break;
+            case L2I:
+                s = "L2I";
+                break;
+            case L2F:
+                s = "L2F";
+                break;
+            case L2D:
+                s = "L2D";
+                break;
+            case F2I:
+                s = "F2I";
+                break;
+            case F2L:
+                s = "F2L";
+                break;
+            case F2D:
+                s = "F2D";
+                break;
+            case D2I:
+                s = "D2I";
+                break;
+            case D2L:
+                s = "D2L";
+                break;
+            case D2F:
+                s = "D2F";
+                break;
+            case I2B:
+                s = "I2B";
+                break;
+            case I2C:
+                s = "I2C";
+                break;
+            case I2S:
+                s = "I2S";
+                break;
+            case LCMP:
+                s = "LCMP";
+                break;
+            case FCMPL:
+                s = "FCMPL";
+                break;
+            case FCMPG:
+                s = "FCMPG";
+                break;
+            case DCMPL:
+                s = "DCMPL";
+                break;
+            case DCMPG:
+                s = "DCMPG";
+                break;
+            case IFEQ:
+                s = "IFEQ";
+                break;
+            case IFNE:
+                s = "IFNE";
+                break;
+            case IFLT:
+                s = "IFLT";
+                break;
+            case IFGE:
+                s = "IFGE";
+                break;
+            case IFGT:
+                s = "IFGT";
+                break;
+            case IFLE:
+                s = "IFLE";
+                break;
+            case IF_ICMPEQ:
+                s = "IF_ICMPEQ";
+                break;
+            case IF_ICMPNE:
+                s = "IF_ICMPNE";
+                break;
+            case IF_ICMPLT:
+                s = "IF_ICMPLT";
+                break;
+            case IF_ICMPGE:
+                s = "IF_ICMPGE";
+                break;
+            case IF_ICMPGT:
+                s = "IF_ICMPGT";
+                break;
+            case IF_ICMPLE:
+                s = "IF_ICMPLE";
+                break;
+            case IF_ACMPEQ:
+                s = "IF_ACMPEQ";
+                break;
+            case IF_ACMPNE:
+                s = "IF_ACMPNE";
+                break;
             case GOTO:
                 s = "GOTO";
                 break;
+            case JSR:
+                s = "JSR";
+                break;
+            case RET:
+                s = "RET";
+                break;
+            case TABLESWITCH:
+                s = "TABLESWITCH";
+                break;
+            case LOOKUPSWITCH:
+                s = "LOOKUPSWITCH";
+                break;
+            case IRETURN:
+                s = "IRETURN";
+                break;
+            case LRETURN:
+                s = "LRETURN";
+                break;
+            case FRETURN:
+                s = "FRETURN";
+                break;
+            case DRETURN:
+                s = "DRETURN";
+                break;
+            case ARETURN:
+                s = "ARETURN";
+                break;
+            case RETURN:
+                s = "RETURN";
+                break;
+            case GETSTATIC:
+                s = "GETSTATIC";
+                break;
+            case PUTSTATIC:
+                s = "PUTSTATIC";
+                break;
+            case GETFIELD:
+                s = "GETFIELD";
+                break;
+            case PUTFIELD:
+                s = "PUTFIELD";
+                break;
+            case INVOKEVIRTUAL:
+                s = "INVOKEVIRTUAL";
+                break;
+            case INVOKESPECIAL:
+                s = "INVOKESPECIAL";
+                break;
+            case INVOKESTATIC:
+                s = "INVOKESTATIC";
+                break;
+            case INVOKEINTERFACE:
+                s = "INVOKEINTERFACE";
+                break;
+            case INVOKEDYNAMIC:
+                s = "INVOKEDYNAMIC";
+                break;
+            case NEW:
+                s = "NEW";
+                break;
+            case NEWARRAY:
+                s = "NEWARRAY";
+                break;
+            case ANEWARRAY:
+                s = "ANEWARRAY";
+                break;
+            case ARRAYLENGTH:
+                s = "ARRAYLENGTH";
+                break;
+            case ATHROW:
+                s = "ATHROW";
+                break;
+            case CHECKCAST:
+                s = "CHECKCAST";
+                break;
+            case INSTANCEOF:
+                s = "INSTANCEOF";
+                break;
+            case MONITORENTER:
+                s = "MONITORENTER";
+                break;
+            case MONITOREXIT:
+                s = "MONITOREXIT";
+                break;
+            case MULTIANEWARRAY:
+                s = "MULTIANEWARRAY";
+                break;
+            case IFNULL:
+                s = "IFNULL";
+                break;
+            case IFNONNULL:
+                s = "IFNONNULL";
+                break;
+
             case 0xFFFFFFFF:
                 s = "FF";
                 break;
