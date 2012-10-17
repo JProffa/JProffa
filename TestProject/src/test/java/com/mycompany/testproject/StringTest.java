acaf0aec08c319533b7088d2917276272fb1975f
Aleksi Majander <aleksi.majander@cs.helsinki.fi>
2012-10-17 09:40:11 +0300

    Modified StringExample to take parameters, added first tests to StringTest

---
+++ TestProject/src/test/java/com/mycompany/testproject/StringTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testproject;
+
+import fi.lolcatz.profiler.ProfileData;
+import fi.lolcatz.profiler.Util;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author oorissan
+ */
+public class StringTest {
+    
+    public StringTest() {
+    }
+    
+    @BeforeClass
+    public static void classSetup() {
+        Example.main(null);
+        Util.loadAgent();      
+        ProfileData.initialize(); 
+        StringExample.stringReplace("aaaaaaaaas", "s", "a");
+    }
+    
+    @Before
+    public void testSetup() {
+        ProfileData.resetCounters();
+    }
+    
+      @Test
+    public void testStringReplaceCostTen() {
+        StringExample.stringReplace("aaaaaaaaas", "s", "a");
+        long totalCost = ProfileData.getTotalCost();
+        assertTrue("The total cost was: " + totalCost, totalCost == 14);
+    }
+    
+    @Test
+    public void testStringReplaceIsDeterministic() {
+        StringExample.stringReplace("aaaaaaaaas", "s", "a");
+        long totalCost = ProfileData.getTotalCost();
+        assertTrue("The total cost was: " + totalCost, totalCost == 14);
+    }
+    
+    @Test
+    /**
+     * should be about linear compared to to testStringReplaceCostTen()
+     */
+    public void testStringReplaceCostHundred() {
+        StringExample.stringReplace("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas", "s", "a");
+        long totalCost = ProfileData.getTotalCost();
+        assertFalse("The total cost was: " + totalCost, totalCost == 14);
+    }
+}
