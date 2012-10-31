5749b471dafc657469d34da306341c59851b68d4
Olli Rissanen <olli.rissanen@helsinki.fi>
2012-10-31 12:43:26 +0200

    Added tests for Native methods

---
+++ TestProject/src/test/java/com/mycompany/testproject/NativeTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testproject;
+
+import fi.lolcatz.profiler.ProfileData;
+import fi.lolcatz.profiler.Util;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author oorissan
+ */
+public class NativeTest {
+    
+    public NativeTest() {
+    }
+    
+    @BeforeClass
+    public static void classSetup() {
+        Example.main(null);
+        Util.loadAgent();      
+        ProfileData.initialize(); 
+    }
+    
+    @Before
+    public void testSetup() {
+        ProfileData.resetCounters();
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() throws IOException {
+         NativeExample example = new NativeExample();
+         example.writeSomething();
+         long cost = ProfileData.getTotalCost();
+         ProfileData.printBasicBlocksCost(false);
+     }
+}
