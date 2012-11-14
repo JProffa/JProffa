a427bd663b15b540b04c5eed6dd53ee340f70647
Olli Rissanen <olli.rissanen@helsinki.fi>
2012-11-14 13:23:17 +0200

    Removed unused methods from Benchmark interface

--- Profiler/src/main/java/fi/lolcatz/profiler/Benchmarkable.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/Benchmarkable.java
@@ -7,6 +7,4 @@ public interface Benchmarkable<T> {
     T getInput(int size);
     long run (T input);
     int getMaxTime(T input, int size);
-    void setMethodName(String name);
-    void setClassName(String name);
 }
