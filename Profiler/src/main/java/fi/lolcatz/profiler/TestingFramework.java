bb7b2d4b12d103e84a477b486b90529db5cb4029
Olli Rissanen <olli.rissanen@helsinki.fi>
2012-11-19 13:20:23 +0200

    Added linearity check

--- Profiler/src/main/java/fi/lolcatz/profiler/TestingFramework.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/TestingFramework.java
@@ -4,18 +4,46 @@
  */
 package fi.lolcatz.profiler;
 
+import java.lang.Math.*;
+
 public class TestingFramework {
 
-    long[][] array;
+    Output<?> out;
 
     public TestingFramework(){        
     }
     
-    public TestingFramework(long[][] array) {
-        this.array = array;
+    public TestingFramework(Output<?> out) {
+        this.out = out;
     }
     
-    public boolean isLinear(long[][] array){
+    /*
+     * Calculates the linearity of the class variable out
+     */
+    public boolean isLinear(){
+        Integer basicSize = out.getSize().get(0);
+        Long basicCost = out.getTime().get(0);      
+        for (int i = 0; i < out.getTime().size(); i++){          
+            double multiplier = out.getSize().get(i)*1.0 / basicSize;
+            Long time = out.getTime().get(i);
+            boolean linearity = Math.abs((multiplier * basicCost) - time) < time*0.2 ? true : false;
+            if (!linearity) return false;     
+        }
+        return true;
+    }
+    
+    /*
+     * Calculates the linearity of the parameter output
+     */
+    public boolean isLinear(Output<?> output){
+        Integer basicSize = output.getSize().get(0);
+        Long basicCost = output.getTime().get(0);      
+        for (int i = 0; i < output.getTime().size(); i++){          
+            double multiplier = output.getSize().get(i) / basicSize;
+            Long time = output.getTime().get(i);
+            boolean linearity = Math.abs((multiplier * basicCost) - time) < time*0.2 ? true : false;
+            if (!linearity) return false;     
+        }
         return true;
     }
     
