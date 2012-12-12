14a644e3ccc21a014750bbb6eecfad9cb40ef70f
Hallo <ville.knuuttila@gmail.com>
2012-12-12 14:15:41 +0200

    Added test to CostlyMethodList

--- TestProject/src/test/java/fi/lolcatz/jproffa/test/NativeMethodCostTest.java
+++ TestProject/src/test/java/fi/lolcatz/jproffa/test/NativeMethodCostTest.java
@@ -4,11 +4,14 @@ import fi.lolcatz.jproffa.testproject.Example;
 import fi.lolcatz.jproffa.testproject.NativeExample;
 import fi.lolcatz.profiledata.ProfileData;
 import fi.lolcatz.profiler.ClassBlacklist;
+import fi.lolcatz.profiler.CostlyMethodList;
 import fi.lolcatz.profiler.Util;
 import java.io.IOException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class NativeMethodCostTest {
     
@@ -34,5 +37,21 @@ public class NativeMethodCostTest {
          example.writeSomething();
          long cost = Util.getTotalCost();
          Util.printBasicBlocksCost(false);
+     }
+     
+     @Test
+     public void testCostlyMethods() throws IOException {
+         NativeExample example = new NativeExample();
+         example.writeSomething();
+         long cost1 = Util.getTotalCost();
+         CostlyMethodList.setCostOfCostlyMethods(0);
+         ProfileData.resetCounters();
+         example = new NativeExample();
+         example.writeSomething();
+         long cost2 = Util.getTotalCost();
+         assertTrue(cost1 > cost2);
+         
+         Util.printBasicBlocksCost(false);
+         
      }
 }
