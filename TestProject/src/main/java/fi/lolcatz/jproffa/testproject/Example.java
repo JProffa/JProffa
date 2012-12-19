5210097b07b6826186cf18fd4fce450093dc35bb
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2012-12-19 16:13:11 +0200

    Readded main

--- TestProject/src/main/java/fi/lolcatz/jproffa/testproject/Example.java
+++ TestProject/src/main/java/fi/lolcatz/jproffa/testproject/Example.java
@@ -2,6 +2,10 @@ package fi.lolcatz.jproffa.testproject;
 
 
 public class Example {
+
+    public static void main(String[] args) {
+        
+    }
     
     public static int sum(int a, int b) {
         return a + b;
