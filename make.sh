f9e833e8cd1b39796c0b0b50f4f7a58d7ab8cc12
Martin Pärtel <martin.partel@gmail.com>
2012-12-21 14:01:52 +0200

    make.sh: use master Maven project.

--- make.sh
+++ make.sh
@@ -2,24 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-cd ProfileData
-mvn clean install -DskipTests
-if [ "$?" -ne "0" ] 
-then 
-    exit
-fi
-cd ../Profiler
-mvn clean install -DskipTests
-if [ "$?" -ne "0" ]
-then
-    exit
-fi
-cd ../Graph
-mvn clean install -DskipTests
-if [ "$?" -ne "0" ]
-then
-    exit
-fi
-cd ../TestProject
 mvn clean install -DskipTests
 
