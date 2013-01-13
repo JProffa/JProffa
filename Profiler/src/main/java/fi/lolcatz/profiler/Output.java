2bfd14a30ca5b0a015e1c4e4d22a622ff464ce4b
Martin Pärtel <martin.partel@gmail.com>
2013-01-13 21:42:28 +0200

    Added nice toString() to Output.

--- Profiler/src/main/java/fi/lolcatz/profiler/Output.java
+++ Profiler/src/main/java/fi/lolcatz/profiler/Output.java
@@ -1,6 +1,7 @@
 package fi.lolcatz.profiler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Output<T> {
@@ -49,6 +50,72 @@ public class Output<T> {
 
     public void addToSize(int i) {
         size.add(i);
+    }
+    
+    public int entryCount() {
+        return size.size();
+    }
+
+    @Override
+    public String toString() {
+        if (input.isEmpty()) {
+            return "<empty Output>";
+        }
+        if (input.size() != size.size() || size.size() != time.size()) {
+            return "<invalid Output>";
+        }
+        
+        String[][] rows = new String[][] {
+            new String[input.size()],
+            new String[size.size()],
+            new String[time.size()]
+        };
+        int n = entryCount();
+        for (int col = 0; col < n; ++col) {
+            rows[0][col] = input.get(col).toString();
+            rows[1][col] = size.get(col).toString();
+            rows[2][col] = time.get(col).toString();
+        }
+        
+        return formatTable(rows);
+    }
+
+    private String formatTable(String[][] rows) {
+        StringBuilder sb = new StringBuilder();
+        int[] colWidths = getColWidths(rows);
+        
+        for (int i = 0; i < rows.length; ++i) {
+            String[] row = rows[i];
+            
+            sb.append("| ");
+            for (int j = 0; j < row.length; ++j) {
+                if (j > 0) {
+                    sb.append(" | ");
+                }
+                
+                int colWidth = colWidths[j];
+                int padding = colWidth - row[j].length();
+                for (int p = 0; p < padding; ++p) {
+                    sb.append(' ');
+                }
+                sb.append(row[j]);
+            }
+            sb.append(" |").append("\n");
+            
+        }
+        return sb.toString();
+    }
+
+    private int[] getColWidths(String[][] rows) {
+        int[] colWidths = new int[entryCount()];
+        for (int j = 0; j < entryCount(); ++j) {
+            int w = 0;
+            for (int i = 0; i < rows.length; ++i) {
+                w = Math.max(w, rows[i][j].length());
+            }
+            colWidths[j] = w;
+        }
+        return colWidths;
     }
 
 }
